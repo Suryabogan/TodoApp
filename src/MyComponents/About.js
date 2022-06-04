a0a685bf3fe5bf87cf9b694c833f4e2df5299174
Surya Shrestha <shresthasurya92@gmail.com>
2022-06-04 20:25:59 +0545

    todoApp

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+    This is an about component
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi asperiores atque explicabo et in ullam eos consequatur. Aspernatur in omnis sequi culpa dolore? Corrupti maiores qui sed perferendis doloremque quia aliquid debitis corporis asperiores?</p>
+    </div>
+  )
+}
